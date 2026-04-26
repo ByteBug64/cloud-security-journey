4f6aaf5df1e9447bf051ff3814afee23bf6059aa
~𝓑𝔂𝓽𝓮𝓑𝓾𝓰 <169054263+ByteBug64@users.noreply.github.com>
2026-04-26 14:24:17 +0100

    Add files via upload

--- projects/05-s3-misconfiguration-scanner/report/generator.py
+++ projects/05-s3-misconfiguration-scanner/report/generator.py
@@ -1,1 +1,26 @@
+from rich.console import Console
+from rich.table import Table
+from jinja2 import Environment, FileSystemLoader
+import os
+from datetime import datetime
 
+console = Console()
+def print_terminal_report(results):
+    table = Table(title="S3 Bucket Security Report")
+    table.add_column("Bucket Name")
+    table.add_column("Check")
+    table.add_column("Status")
+    table.add_column("Issues")
+    for result in results:
+        for finding in result['findings']:
+            table.add_row(result['bucket'], finding['check'], finding['status'],",".join(finding['issues']))
+    console.print(table)
+def generate_htmlreport(results):
+    env = Environment(loader = FileSystemLoader(os.path.dirname(__file__)))
+    template = env.get_template('template.html')
+    html_output = template.render(results=results)
+    timestamp = f"report_{datetime.now().strftime('%Y%m%d_%H%M%S')}.html"
+    output_path = os.path.join('output', timestamp )
+    with open(output_path, 'w') as f:
+        f.write(html_output)
+    print(f"Report saved to {output_path}")
